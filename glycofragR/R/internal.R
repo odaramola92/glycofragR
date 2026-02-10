6cab6c0bb45cc19c1cbbbcdc0b153cd09c9b8c79
Oluwatosin Emmanuel Daramola <156952585+odaramola92@users.noreply.github.com>
2026-02-09 20:19:22 -0600

    Delete glycofragR directory

--- glycofragR/R/internal.R
+++
@@ -1,6 +0,0 @@
-.require_py <- function() {
-  if (!reticulate::py_available(initialize = FALSE)) {
-    stop("Python is not available. Use set_glycofrag_conda_env() first.", call. = FALSE)
-  }
-  invisible(TRUE)
-}
